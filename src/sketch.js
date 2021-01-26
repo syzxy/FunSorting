bb30fd0f669c4f9b12bfd589ada6a7fe59160399
Sam Shi <yunzeshi@outlook.com>
2021-01-26 17:57:30 +1100

    color scheme adjusted

--- src/sketch.js
+++ src/sketch.js
@@ -7,9 +7,9 @@
 const debugElements = [70, 180, 150, 90, 80, 100, 60, 40];
 let debugMode = false;
 let currentAlgorithm;
-let barWidth = 3;
-let barInterval = 1;
-let visualSpeed = 10;
+let barWidth = 4;
+let barInterval = 1.1;
+let visualSpeed = 50;
 let elements = debugMode ? [...debugElements] : [];
 let colors;
 // let dimmed;             // 1: dim the color of the ith bar, 0: otherwise
