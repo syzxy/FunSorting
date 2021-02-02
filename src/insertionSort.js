80f5a55f753061bbb3ef1cdedbc2597e6d66f32c
Sam Shi <yunzeshi@outlook.com>
2021-02-02 12:35:32 +1100

    complete laste step for insertion sort

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -9,7 +9,7 @@ class InserstionSort extends Sort {
   }
 
   sort() {
-    this.steps.push({action: "sorted", idx: 0});
+    this.steps.push({action: "sort", idx: 0});
     for (let i = 1; i < this.arr.length; i++) {
       this.steps.push({action: "activate", idx: i});
       let j = i;
@@ -18,11 +18,11 @@ class InserstionSort extends Sort {
         this.steps.push({action: "swap", from: j, to: j - 1});
         j--;
       }
-      this.steps.push({action: "sorted", idx: j});
+      this.steps.push({action: "sort", idx: j});
     }
   }
 
-  animateStep(stepIndex) {
+  animateStep(stepIndex, forwardMode=true) {
     if (stepIndex === this.steps.length - 1) {
       clearInterval(this.timer);
       this.finished = true;
@@ -30,17 +30,18 @@ class InserstionSort extends Sort {
     }
 
     let step = this.steps[stepIndex];
+    console.log(step);
     switch (step.action) {
-      case 'sorted':
-        this.states[step.idx] = 'sorted';
+      case 'sort':
+        this.states[step.idx] = forwardMode ? 'sorted' : (step.idx === 0 ? 'default': 'compared');
         break;
       case 'activate':
-        this.states[step.idx] = 'activated';
+        this.states[step.idx] = forwardMode ? 'compared' : 'default';
         break;
       case 'swap':
         [this.arr[step.from], this.arr[step.to]] = [this.arr[step.to], this.arr[step.from]];
-        this.states[step.from] = 'sorted';
-        this.states[step.to] = 'activated';
+        this.states[step.from] = forwardMode ? 'sorted' : 'compared';
+        this.states[step.to] = forwardMode ? 'compared' : 'sorted';
         break;
     }
   }
